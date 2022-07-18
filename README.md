719b6ffc15a1edc41e842f0b78671053ff43e0ee
Khushnid <99318942+chowiskhush@users.noreply.github.com>
2022-07-18 18:28:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AtlassianHelper
+ This repository is created for simple usage for working with Jira and Trello APIs
---
+++ Tests/AtlassianHelperTests/AtlassianHelperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AtlassianHelper
+
+final class AtlassianHelperTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AtlassianHelper().text, "Hello, World!")
+    }
+}
