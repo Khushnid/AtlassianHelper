f47d4fca51cac2fe334117ba833ef49deda0f309
Khushnid <99318942+chowiskhush@users.noreply.github.com>
2022-07-20 22:24:53 +0500

    Changing multiLine code into single

--- Sources/AtlassianHelper/Main/JiraHelper/Helper/TicketData.swift
+++ Sources/AtlassianHelper/Main/JiraHelper/Helper/TicketData.swift
@@ -2,38 +2,15 @@ import Foundation
 
 extension JiraService {
     func addTicketData(_ projectKey: String, _ summary: String, _ description: String) -> Data? {
-        let body =
-"""
-    {
-        "fields": {
-            "project": {
-                "key": "\(projectKey)"
-            },
-            "summary": "\(summary)",
-            "description": {
-                "type": "doc",
-                "version": 1,
-                "content": [
-                    {
-                        "type": "paragraph",
-                        "content": [
-                            {
-                                "type": "text",
-                                "text": "\(description)"
-                            }
-                        ]
-                    }
-                ]
-            },
-            "issuetype": {
-                "name": "Task"
-            }
-        }
-    }
-"""
+        
+        let jsonBody: String = {
+               """
+                  {"fields":{"project":{"key":"\(projectKey)"},"summary":"\(summary)","description":{"type":"doc","version":1,"content":[{"type":"paragraph","content":[{"type":"text","text":"\(description)"}]}]},"issuetype":{"name":"Task"}}}
+               """
+        }()
         
         do {
-            if let json = body.data(using: String.Encoding.utf8){
+            if let json = jsonBody.data(using: String.Encoding.utf8){
                 if let jsonData = try JSONSerialization.jsonObject(with: json, options: .allowFragments) as? [String:AnyObject] {
                     return try? JSONSerialization.data(withJSONObject: jsonData, options: [])
                 }
